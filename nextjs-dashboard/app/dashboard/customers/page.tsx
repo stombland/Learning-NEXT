09ee56caa37a61e49c7883d53111fd17a2d441c0
King <stombland@gmail.com>
2024-12-27 05:26:13 +0000

    ch5

---
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+
+
+function customers(){
+    return(
+        <p> customers page </p>
+    )
+}
+
+export default customers;
---
+++ nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+
+
+function invoices(){
+    return(
+        <h1> invoices page </h1>
+    )
+}
+
+export default invoices;
